979b98da97250d8145ef24a891478c34dff4a445
Jason Wei <jason.william.wei@gmail.com>
2026-02-07 01:11:56 +0000

    update algorithm configs

--- verl/trainer/config/algorithm.py
+++ verl/trainer/config/algorithm.py
@@ -603,7 +603,9 @@ class AlgoConfig(BaseConfig):
     lam: float = 1.0
     adv_estimator: str = "gae"
     norm_adv_by_std_in_grpo: bool = True
-    linear: bool = False
+    eca_linear: bool = False
+    eca_softmax: bool = False
+    eca_gamma: float = 1.0
     use_kl_in_reward: bool = False
     kl_penalty: str = "kl"
     kl_ctrl: KLControlConfig = field(default_factory=KLControlConfig)
