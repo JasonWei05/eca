c61d3e9968e84bbe25d0ae84bed76d9c884643bc
qinbo.bai <qinbo.bai@bytedance.com>
2026-02-19 00:38:28 +0000

    add math_verify to setup.py

--- setup.py
+++ setup.py
@@ -42,6 +42,7 @@ install_requires = [
     "wandb",
     "packaging>=20.0",
     "tensorboard",
+    "math_verify",
 ]
 
 TEST_REQUIRES = ["pytest", "pre-commit", "py-spy", "pytest-asyncio", "pytest-rerunfailures"]
